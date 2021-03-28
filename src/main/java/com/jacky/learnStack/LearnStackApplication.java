feca69f1f8e0c95c6497fbf1695b7fbfe58db506
Jacky <964413011@qq.com>
2021-03-28 23:26:22 +0800

    'init'

---
+++ src/main/java/com/jacky/learnStack/LearnStackApplication.java
@@ -0,0 +1,13 @@
+package com.jacky.learnStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacky/learnStack/LearnStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacky.learnStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
